4922b7975a5a303a8c2e224b406694035f4c389d
Marcelo1701 <marcelo.garcia1701@gmail.com>
2023-09-13 18:37:08 -0300

    Alumno data
    actualización rama Marcelo

---
+++ AppGestionULP/src/AccesoADatos/AlumnoData.java
@@ -0,0 +1,44 @@
+
+package AccesoADatos;
+
+import AccesoADatos.Conexion;
+import AccesoADatos.Conexion;
+import Entidades.Alumno;
+import java.sql.Connection;
+import java.util.List;
+
+
+public class AlumnoData {
+    
+    private Connection con = null;
+    
+    
+    
+    public AlumnoData(){}
+        
+       
+       
+    public void guardarAlumno(Alumno alumno) {}
+
+
+    public Alumno buscarAlumno(int id) {return null;
+}
+
+
+    public Alumno buscarAlumnoPorDni(int dni) {return null;
+}
+
+
+    public List<Alumno> listarAlumnos() {return null;
+}
+
+
+    public void modificarAlumno(Alumno alumno){}
+
+
+    public void eliminarAlumno(int id) {}
+
+       
+       
+       
+}
